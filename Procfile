f583060d0aae96c00b4c946229a7ea732134293f
Ahmed Anwar <ameeahmedy@gmail.com>
2025-11-02 10:32:48 +0100

    Fix database configuration

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && gunicorn ussd_helper.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn ussd_helper.wsgi:application --bind 0.0.0.0:$PORT
--- ussd_helper/settings.py
+++ ussd_helper/settings.py
@@ -86,6 +86,12 @@ DATABASES = {
     }
 }
 
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
